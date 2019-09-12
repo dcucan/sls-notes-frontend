d43e7e71e3f10570b5e3b6b44a344a9c7dcb5540
z <drozdova.zuzka@seznam.cz>
2019-09-12 17:19:38 +0000

    Edited webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,8 +5,8 @@ var ngw = require('@ngtools/webpack');
 
 const path = require('path');
 
-const API_ROOT = 'https://api.zuzanadrozdova.com'; // CHANGE THIS TO MATCH THE URL OF YOUR API GATEWAY API OR YOUR CUSTOM DOMAIN FOR API
-const STAGE = 'awswebapp'; // CHANGE THIS TO MATCH THE STAGE OF YOUR API OR BASEPATH OF YOUR CUSTOM DOMAIN FOR API e.g. /prod OR /v1
+const API_ROOT = 'https://api.zuzanadrozdova.com/awswebapp'; // CHANGE THIS TO MATCH THE URL OF YOUR API GATEWAY API OR YOUR CUSTOM DOMAIN FOR API
+const STAGE = ''; // CHANGE THIS TO MATCH THE STAGE OF YOUR API OR BASEPATH OF YOUR CUSTOM DOMAIN FOR API e.g. /prod OR /v1
 const METADATA = webpackMerge(commonConfig.metadata, {
     API_ROOT: API_ROOT,
     STAGE: STAGE
